1d7d386a7348d871eeeb2d3689e624cbf2c4f5c0
chakrit tnasinpaibool <newchakrit@Administrators-MacBook-Pro-7.local>
2022-07-07 16:44:30 +0700

    pointer

--- overview-of-the-go/Pointers/main.go
+++ overview-of-the-go/Pointers/main.go
@@ -1,5 +1,21 @@
 package main
 
+import "log"
+
 func main () {
+	var myString string
+	myString = "Green"
+
+	log.Println("My string is set to", myString)
 	
+	changeUsingPointer(&myString)
+
+	log.Println("After func call myString is set to", myString)
+}
+
+func changeUsingPointer (s *string) {
+	log.Println("s is set to", s)
+
+	newValue := "Red"
+	*s = newValue
 }
