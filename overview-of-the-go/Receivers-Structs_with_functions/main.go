56346d233bebeb736d3d9216ba5aa01f6c1a634a
chakrit tnasinpaibool <imnew108@gmail.com>
2022-07-07 17:16:00 +0700

    receivers struck with func

--- overview-of-the-go/Receivers-Structs_with_functions/main.go
+++ overview-of-the-go/Receivers-Structs_with_functions/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "log"
 
+
+type myStruck struct{
+	Firstname string
+}
+
+func (m *myStruck) printFirstName () string {
+	return m.Firstname
+}
+
+func main() {
+	var myVar myStruck
+	myVar.Firstname = "New"
+
+	myVar2 := myStruck{
+		Firstname: "Oum",
+	}
+
+	log.Println("myVar is set to", myVar.printFirstName())
+	log.Println("myVar2 is set to", myVar2.printFirstName())
 }
